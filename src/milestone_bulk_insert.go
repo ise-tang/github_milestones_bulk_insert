3f42d6cb4e12367501872f326da1a3d38423e0c1
issei126 <issei.nakamura@peroli.jp>
2019-03-30 21:16:56 +0900

    auto format

--- src/milestone_bulk_insert.go
+++ src/milestone_bulk_insert.go
@@ -1,52 +1,52 @@
 package main
 
 import (
+	"context"
+	"encoding/csv"
+	"fmt"
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
-  "gopkg.in/yaml.v2"
-  "io/ioutil"
-	"context"
-	"os"
+	"gopkg.in/yaml.v2"
 	"io"
-	"encoding/csv"
+	"io/ioutil"
+	"os"
 	"time"
-	"fmt"
 )
 
 func main() {
 	// Load setting.yml
 	buf, err := ioutil.ReadFile("settings.yml")
 	if err != nil {
-    return
+		return
 	}
 
-  m := make(map[interface{}]interface{})
-  err = yaml.Unmarshal(buf, &m)
-  if err != nil {
-    panic(err)
-  }
+	m := make(map[interface{}]interface{})
+	err = yaml.Unmarshal(buf, &m)
+	if err != nil {
+		panic(err)
+	}
 
 	// create GitHub client
 	ctx := context.Background()
-  ts := oauth2.StaticTokenSource(
-    &oauth2.Token{AccessToken: m["access_token"].(string)},
-  )
-  tc := oauth2.NewClient(oauth2.NoContext, ts)
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: m["access_token"].(string)},
+	)
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
 
-  client := github.NewClient(tc)
+	client := github.NewClient(tc)
 
-  // read CSV
-  var fp *os.File
-  if len(os.Args) < 2 {
-    fp = os.Stdin
-  } else {
-    var err error
-    fp, err = os.Open(os.Args[1])
-    if err != nil {
-    				panic(err)
-    }
-    defer fp.Close()
-  }
+	// read CSV
+	var fp *os.File
+	if len(os.Args) < 2 {
+		fp = os.Stdin
+	} else {
+		var err error
+		fp, err = os.Open(os.Args[1])
+		if err != nil {
+			panic(err)
+		}
+		defer fp.Close()
+	}
 
 	reader := csv.NewReader(fp)
 	reader.Comma = ','
@@ -57,23 +57,23 @@ func main() {
 	for {
 		record, err := reader.Read()
 		if err == io.EOF {
-						break
+			break
 		} else if err != nil {
-						panic(err)
+			panic(err)
 		} else if header == nil {
 			header = record
 			continue
 		}
 		due_on, err := time.Parse(layout, record[2])
 		if err != nil {
-		  panic(err)
+			panic(err)
 		}
 
-		milestone :=  github.Milestone {
-			Title: github.String(record[0]),
+		milestone := github.Milestone{
+			Title:       github.String(record[0]),
 			Description: github.String(record[1]),
-			DueOn: &due_on,
-    }
+			DueOn:       &due_on,
+		}
 		// create milestone
 		repos, _, err := client.Issues.CreateMilestone(ctx, m["owner"].(string), m["repo"].(string), &milestone)
 
